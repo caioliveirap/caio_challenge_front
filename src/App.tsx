c7f6cedc3860d41ed58a33dd3da27fc8c4b6f250
caioliveirap <caio191201@gmail.com>
2023-03-28 01:54:00 -0300

    feat: added the antd package to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { ConfigProvider } from 'antd';
+import ptBR from 'antd/locale/pt_BR';
+import 'antd/dist/reset.css';
+import { DatePicker, message } from 'antd';
 
 function App() {
-	const [count, setCount] = useState(0);
-
+	const handleChange = (value: any) => {
+		message.info(
+			`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`
+		);
+	};
 	return (
-		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</div>
+		<ConfigProvider locale={ptBR}>
+			<DatePicker onChange={handleChange} />
+		</ConfigProvider>
 	);
 }
 
