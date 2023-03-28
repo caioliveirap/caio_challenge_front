8ff4574ad4a2a6714187e5c942a5be3fe5aa86df
caioliveirap <caio191201@gmail.com>
2023-03-28 02:15:33 -0300

    feat: created project routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const settedRoutes = createBrowserRouter([
+	{
+		path: '/',
+		element: <span>test</span>,
+	},
+]);
+
+export const ProjectRoutes = () => {
+	return <RouterProvider router={settedRoutes} />;
+};
