f31d963f9eace1c2073ff3d6aa9e0ee6388143b4
caioliveirap <caio191201@gmail.com>
2023-03-28 03:05:25 -0300

    feat: added poppins font and content wrapper on project

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,9 @@ function App() {
 							setCollapsed(!isSidenavCollapsed);
 						}}
 					/>
+					<ProjectRoutes />
 				</Layout>
 			</Layout>
-			{/* <ProjectRoutes /> */}
 		</ConfigProvider>
 	);
 }
